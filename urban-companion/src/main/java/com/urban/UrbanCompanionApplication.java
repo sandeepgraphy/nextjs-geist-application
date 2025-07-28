566a8946b9acd081b8422b8cac6ecf4914d7d540
Sandeep Kumar <152271640+sandeepgraphy@users.noreply.github.com>
2025-07-28 13:28:43 +0530

    Add urban-companion/src/main/java/com/urban/UrbanCompanionApplication.java 1753689525104

---
+++ urban-companion/src/main/java/com/urban/UrbanCompanionApplication.java
@@ -0,0 +1,11 @@
+package com.urban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanCompanionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UrbanCompanionApplication.class, args);
+    }
+}
