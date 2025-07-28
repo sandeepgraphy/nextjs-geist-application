197726667d44dc07006179aaac4acf2cfd4053f5
Sandeep Kumar <152271640+sandeepgraphy@users.noreply.github.com>
2025-07-28 13:28:35 +0530

    Add urban-companion/src/main/java/com/urban/repository/mysql/UserRepository.java 1753689517621

---
+++ urban-companion/src/main/java/com/urban/repository/mysql/UserRepository.java
@@ -0,0 +1,10 @@
+package com.urban.repository.mysql;
+
+import com.urban.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
